00cf55907edaaaff7ebbb9a86c4120290e496fde
awaqo <aqiljawadalfurqon@gmail.com>
2018-09-17 00:04:21 +0700

    Login fb first

--- App.js
+++ App.js
@@ -7,28 +7,20 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
-import Judul from './Component/Judul';
+import {StyleSheet, View, StatusBar} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+import Login from './Pages/Login';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Judul title="BIODATA"/>
-        <Judul title="LOGIN"/>
-        <Text>Nama : Aqil Jawadal Furqon</Text>
-        <Text>Kelas : XI RPL 3</Text>
-        <Text>No Absen : 08</Text>
-        <Image style={styles.Image} source={require('./img/a.jpeg')} />
-      </View>
+          <View style={styles.container}>
+            <StatusBar
+              backgroundColor="#1c313a"
+              barStyle="light-content"
+            />
+            <Login/>
+          </View>
     );
   }
 }
@@ -36,22 +28,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor:'#455a64',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  Image: {
-    width : 250,
-    height : 250,
-  },
-});
+  });
